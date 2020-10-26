c4e52bca4427b34604a3ced085b55122967d6dd8
mrtvirus <mrtvirus@kali.mrtvirushub.com>
2020-10-26 17:57:38 +0600

    efficient helpfulmath

--- Helpful Maths.py
+++ Helpful Maths.py
@@ -1,17 +1,15 @@
 math_eqn = input()
 lst = []
-lst2 = []
+new_string = ""
 a = b = c = 0
 for i in range(0, len(math_eqn)):
     if(math_eqn[i] == "1" or math_eqn[i] == "2" or math_eqn[i] == "3"):
         lst.append(int(math_eqn[i]))
 
 lst.sort()
-n = len(lst)
-for i in range(0, n):
-    lst2 = str(lst[i])+"+"
 
-for i in range(0, n-1):
-    print(str(lst[i])+"+", end="")
+for i in range(len(lst)):
+    new_string = new_string + str(lst[i]) + "+"
 
-print(lst2[:-1])
+
+print(new_string[:-1])
